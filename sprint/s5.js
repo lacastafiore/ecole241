733aa0044938cfc7db7d27c83d83bb78b159ca2c
Grace Kamgoua <kamgoua2000@gmail.com>
2019-02-18 14:07:53 +0100

    s5

---
+++ sprint/s5.js
@@ -0,0 +1,8 @@
+const nbr1 = prompt("Entrez un nombre:");
+const nbr2 = prompt("Entrez un autre:");
+if (nbr1 > nbr2) {
+    if (nbr1 > nbr2) {
+        console.log(nbr1);
+    } else {
+        console.log(nbr2);
+    } 
