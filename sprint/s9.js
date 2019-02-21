9fa8f5b7e238fbe4793d48edaa2b214a2d956107
Grace Kamgoua <kamgoua2000@gmail.com>
2019-02-21 22:05:28 +0100

    s9 version longue

---
+++ sprint/s9.js
@@ -0,0 +1,32 @@
+let max = 5;
+let nbr1 = prompt("Entrez le nombre numéro 1:");
+if (nbr1 > max) {
+    max = nbr1;
+} else {
+    max = max;
+}
+let nbr2 = prompt("Entrez le nombre numéro 2:");
+if (nbr2 > max) {
+    max = nbr2;
+} else {
+    max = max;
+}
+let nbr3 = prompt("Entrez le nombre numéro 3:");
+if (nbr3 > max) {
+    max = nbr3;
+} else {
+    max = max;
+}
+let nbr4 = prompt("Entrez le nombre numéro 4:");
+if (nbr4 > max) {
+    max = nbr4;
+} else {
+    max = max;
+}
+let nbr5 = prompt("Entrez le nombre numéro 5:");
+if (nbr5 > max) {
+    max = nbr5;
+} else {
+    max = max;
+}
+console.log(`Le plus grand de ces nombre est: ${max}`);
