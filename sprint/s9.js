88878e899ed4f2a4bb69b9b26f7bc252e51ba778
Grace Kamgoua <kamgoua2000@gmail.com>
2019-03-01 01:43:49 +0100

    new profil, old in lab (construction). Updated agricollect.

--- sprint/s9.js
+++ sprint/s9.js
@@ -1,7 +1,6 @@
 let max = 5, nbr, i;
 for (i = 1; i < 6; i++) {
   nbr = prompt(`"Entrez le nombre ${i}:"`);
-   if (nbr > max) {
-    max = nbr;
-  } 
-} console.log(`${max}`);
+   if (nbr > max) max = nbr;
+} 
+console.log(`${max}`);
